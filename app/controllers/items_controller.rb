f86f499bbc51f11190c358a28665b6be8ca4eef4
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-06-10 14:42:59 +0900

    投稿機能・編集機能

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,50 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :audio, :category_id, :url, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+end
---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :audio
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
+
+  mount_uploader :audio, AudiofileUploader
+  
+end
