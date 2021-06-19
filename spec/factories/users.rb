06454718971081c7ff57fecadb23b0be9a832539
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-06-19 15:47:51 +0900

    ユーザーモデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/maic.png'), filename: 'test_image.png')
+    end
+  end
+end
