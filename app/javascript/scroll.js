eb50a6f6d4a2b2d02cd9aa4054a068badff57d0e
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-06-24 18:10:23 +0900

    スクロールアニメーション

---
+++ app/javascript/scroll.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+
+ 
+      ScrollReveal().reveal('.list',{
+        duration: 1400, 
+        viewFactor: 0.6, 
+        opacity:0,
+        origin:'top',
+        mobile: 'true',
+        reset: 'true'
+      });
+      
+    
+  })
+  
