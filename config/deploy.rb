c2bf1f827ef1a334931c21710d818d1d90983444
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-06-26 17:33:33 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+
+lock "~> 3.16.0"
+
+set :application, "Review"
+set :repo_url, "git@github.com:sugiryouta/Review.git"
+
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.6' 
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/cogi.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
